4a335534fb4e5ea2fc9f4b2ca365c7c57e134a92
Alexander Dawson <a.m.dawson87@hotmail.com>
2022-04-29 11:25:12 -0700

    teamid

--- seeds/playerData.js
+++ seeds/playerData.js
@@ -2,6 +2,7 @@ const { Player } = require('../models');
 
 const Playerdata = [
   {
+    id: 0,
     username: 'Don_gothands',
     email: 'don@don.com',
     password: 'phx12345',
@@ -10,30 +11,34 @@ const Playerdata = [
    
   },
   {
+    id:1,
     username: 'Alex.30',
     email: 'alex@alex30.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 2,
     
    
   },
   {
+    id:2,
     username: 'Taylor_gotspeed',
     email: 'taylor@taylor.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 3,
     
    
   },
   {
+    id:3,
     username: 'Paul_enforcer',
     email: 'paul@paul.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 4,
   
    
   },
   {
+    id: 4,
     username: 'Ant_thelockdown',
     email: 'ant@antonio.com',
     password: 'phx12345',
@@ -42,30 +47,34 @@ const Playerdata = [
    
   },
   {
+    id: 5,
     username: 'Blossoming Apricot',
     email: 'LedyX',
     password: 'March 30, 2018',
-    team_id: 1,
+    team_id: 2,
   
    
   },
   {
+    id: 6,
     username: 'killer_james',
     email: 'james@james.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 3,
     
    
   },
   {
+    id: 7,
     username: 'k-watts',
     email: 'kyle-watts@kyle.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 4,
     
    
   },
   {
+    id: 8,
     username: 'the2nonlybRell',
     email: 'rell@rell.com',
     password: 'phx12345',
@@ -77,7 +86,7 @@ const Playerdata = [
     username: '_slickRick',
     email: 'ricardo@rick.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 2,
     
    
   },
@@ -85,7 +94,7 @@ const Playerdata = [
     username: 'super_nikki',
     email: 'nikki@nikki.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 3,
     
    
   },
@@ -93,7 +102,7 @@ const Playerdata = [
     username: 'hiflying_Latoya',
     email: 'LadyX@toya.com',
     password: 'phx12345',
-    team_id: 1,
+    team_id: 4,
     
    
   },
